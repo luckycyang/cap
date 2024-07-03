d6ebc27f1d124a0cbe9cfad577ae3a11b5dbe472
dingduck <kfove.liang@gmail.com>
2024-07-03 12:40:36 +0800

    simpel test

--- src/main/scala/core/BasicTestSpec.scala
+++ src/main/scala/core/BasicTestSpec.scala
@@ -1,5 +1,6 @@
 package core
 import chisel3._
+import chisel3.util._
 
 
 
@@ -12,6 +13,31 @@ class BasicModule extends Module {
   out := in  
 }
 
+
+class Operation(mode: Boolean) extends Module {
+  val in0, in1 = IO(Input(UInt(16.W)))
+  val out = IO(Output(UInt(16.W)))
+  
+  if (mode) {
+    out := in0 + in1
+  } else {
+    out := in0 - in1
+  }
+}
+
+class OperationTestSpec() extends Module {
+  val in0, in1 = IO(Input(UInt(16.W)))
+  val out = IO(Output(UInt(16.W)))
+  val sel = IO(Input(Bool()))
+  val op0 = Module(new Operation(true))
+  val op1 = Module(new Operation(false))
+  in0 <> op0.in0
+  in0 <> op1.in0
+  in1 <> op0.in1
+  in1 <> op1.in1
+  out := MuxLookup(sel, in0)(Seq(false.B -> op0.out, true.B -> op1.out))
+}
+
 object Basic extends App {
-  println(getVerilogString(new BasicModule))
+  println(getVerilogString(new OperationTestSpec))
 }
