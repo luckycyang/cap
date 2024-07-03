a98ae32d0aa010ba023b2d510d102fdc62849eeb
dingduck <kfove.liang@gmail.com>
2024-07-03 19:58:32 +0800

    Support mill to test 'mill cap.test'

--- src/test/scala/core/BasicSpec.scala
+++ src/test/scala/core/BasicSpec.scala
@@ -8,13 +8,17 @@ class BasicTestSpec extends AnyFlatSpec with ChiselScalatestTester {
   // test class body here
   it should "pass in this simple env" in {
     println("\u001B[32m\n**************\n基本环境测试通过......\n**************")
-    test(new BasicModule).withAnnotations(Seq(WriteVcdAnnotation)) {
+    test(new OperationTestSpec).withAnnotations(Seq(WriteVcdAnnotation)) {
       dut => {
         step(1)
-        dut.in.poke(true.B)
-        step(1)
-        dut.in.poke(false.B)
-        
+        for (i <- Seq(4, 8); j <- Seq(8, 6)) {
+          dut.in0.poke(i.U)
+          dut.in1.poke(j.U)
+          dut.sel.poke(true.B)
+          step(1)
+          dut.sel.poke(false.B)
+          step(1)
+      }
       }
     }
   }
