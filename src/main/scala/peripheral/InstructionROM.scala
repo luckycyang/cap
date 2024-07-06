cf2d925813c636c50432404aaee191d5d49140bb
dingduck <kfove.liang@gmail.com>
2024-07-06 19:18:26 +0800

    待完工

--- src/main/scala/peripheral/InstructionROM.scala
+++ src/main/scala/peripheral/InstructionROM.scala
@@ -1,1 +1,15 @@
 package peripheral
+
+
+import chisel3._
+import settings.Settings
+
+class InstructionROM(instructionFilename: String) extends Module {
+  val io = IO(new Bundle {
+    val address = Input(UInt(Settings.AddrWidth))
+    val data    = Output(UInt(Settings.InstructionWidth))
+  })
+  io.data := io.address
+  val capacity = 128
+}
+
